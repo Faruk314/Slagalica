7bd09e49e0b0926a05b1d7d960465828ad3a7ea9
Faruk Spahic <farukspahictz@gmail.com>
2023-06-08 14:19:13 +0200

    Created route for MojBroj game

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import Asocijacije from "./pages/Asocijacije";
+import MojBroj from "./pages/MojBroj";
 import Skocko from "./pages/Skocko";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/skocko" element={<Skocko />} />
         <Route path="/asocijacije" element={<Asocijacije />} />
+        <Route path="/mojbroj" element={<MojBroj />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/MojBroj.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MojBroj = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-3"></div>
+
+      <div></div>
+    </section>
+  );
+};
+
+export default MojBroj;
