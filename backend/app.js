249a074761cf97a722ec25045b88914b32dcecf0
Faruk Spahic <farukspahictz@gmail.com>
2023-06-13 20:21:14 +0200

    Added cors and custom error handler in backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,25 @@
 import express from "express";
+import wordRoutes from "./routes/wordGame.js";
+import errorHandler from "./utils/error.js";
+import cors from "cors";
 
 const port = 4000;
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/wordgame", wordRoutes);
+
 app.listen(port, () => {
   console.log("123");
 });
+
+app.use(errorHandler);
---
+++ backend/utils/error.js
@@ -0,0 +1,11 @@
+const errorHandler = async (err, req, res, next) => {
+  const statusCode = req.statusCode ? req.statusCode : 500;
+
+  res.status(statusCode);
+
+  res.json({
+    message: err.message,
+  });
+};
+
+export default errorHandler;
